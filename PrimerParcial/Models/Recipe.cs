edc9e9c2ae6fe82dcf7e83ba8f2f5a618493b5f7
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-09-25 19:35:20 -0400

    entra_a_la_pagina

---
+++ PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
+{
+    public class Recipe
+    {
+        // habia 4 errores y creando el archivo Models/Recipe.cs se corrigieron creando lo necesario a este archivo, ya entra a la pagina
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    }
+}
