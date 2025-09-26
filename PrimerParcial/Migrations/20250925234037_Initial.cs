7bc3472d39bd2f1ec795a4ee8bcdf73cc9f63cc0
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-09-25 20:03:45 -0400

    creado_scaffolding

---
+++ PrimerParcial/Migrations/20250925234037_Initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrimerParcial.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Instructions",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "PreparationTimeMinutes",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "Servings",
+                table: "Recipes");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Recipes",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Recipes",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Recipes",
+                newName: "Title");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Recipes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instructions",
+                table: "Recipes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PreparationTimeMinutes",
+                table: "Recipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Servings",
+                table: "Recipes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
